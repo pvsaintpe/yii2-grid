3aefb6ac6463be23773bedc8841483ac93945b98
Veselov Pavel <veselov@mbk-capital.ru>
2018-10-25 14:55:46 +0300

    fix body asset

--- assets/clickable.js
+++ assets/clickable.js
@@ -1,5 +1,5 @@
 $(function() {
-    $('.clickable-row>td').on('click', function(e) {
+    $('body').on('click', '.clickable-row>td', function(e) {
         if ($(this).get(0).dataset.columnClickable != 0) {
             $('.clickable-row').removeClass('active');
             $(this).parent().addClass('active');
